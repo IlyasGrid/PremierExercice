7ccf6ea90be69f7e3bbd7e8d293d81bdf0056a66
Admin <Admin@DESKTOP-IOMF4D2>
2022-10-25 10:36:25 +0100

    nombre premier

--- PremierExercice/Program.cs
+++ PremierExercice/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 
-namespace PremierExercice 
+namespace PremierExercice
 {
-    internal class Program
+    public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            bool isNumeric;
+            int realNbr; // ou on va stocker le nbr
+            /*bool estPremier = true;*/
+            do
+            {
+                Console.WriteLine("Entrez un nombre");
+                string strnumber = Console.ReadLine();
+                isNumeric = int.TryParse(strnumber, out realNbr);
+                
+                Console.WriteLine();
+
+            } while (!isNumeric);
+
+
+
+
+            if (EstPremier(realNbr))
+            {
+                Console.WriteLine(realNbr+ " est premier");
+            }
+            else
+            {
+                Console.WriteLine(realNbr + " est non premier");
+            }
+
+            Console.ReadKey();
+        }
+
+
+        public static bool EstPremier(int number)
+        {
+            if (number <= 2)
+            {
+                return true;
+            }
+            else
+            {
+
+                double sqrtNbr = Math.Sqrt(number);
+
+                for (int i = 2; i <= sqrtNbr; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
     }
 }
